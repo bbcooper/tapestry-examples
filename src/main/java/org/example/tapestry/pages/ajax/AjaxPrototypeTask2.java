f5736f15fda1e73fe72983e9728b0c138d421330
borut.bolcina <borut.bolcina@ac6f06fe-ab3b-0410-aaaf-636bd12ddef9>
2007-10-07 21:34:29 +0000

    Initial import.

---
+++ src/main/java/org/example/tapestry/pages/ajax/AjaxPrototypeTask2.java
@@ -0,0 +1,69 @@
+package org.example.tapestry.pages.ajax;
+
+import java.util.HashMap;
+
+import org.apache.tapestry.Asset;
+import org.apache.tapestry.ComponentResources;
+import org.apache.tapestry.Link;
+import org.apache.tapestry.StreamResponse;
+import org.apache.tapestry.annotations.Inject;
+import org.apache.tapestry.annotations.Path;
+import org.apache.tapestry.util.TextStreamResponse;
+
+public class AjaxPrototypeTask2 {
+	
+	@Inject
+	@Path("${tapestry.scriptaculous}/prototype.js")
+	private Asset _prototype;
+	
+	private HashMap<String, String> users = new HashMap<String, String>();
+
+	void pageLoaded() {
+		System.out.println("AjaxTests PageLoaded");
+		users.put("greg", "account 1");
+		users.put("duke", "account 2");
+	}
+
+	@Inject
+	private ComponentResources _resources;
+
+	/**
+	 * Generates a URI to the server-side function for the XHR to use.
+	 * 
+	 * @return the link
+	 */
+	public String getTheLink() {
+		Link l = _resources.createActionLink("myAction", false);
+		return l.toURI();
+	}
+
+	/**
+	 * This is a server-side method called via XHR that returns some text.
+	 * 
+	 * @return some text
+	 */
+	StreamResponse onMyAction(String id) {
+		String message;
+		if ((id != null) && users.containsKey(id.trim())) {
+			message = "<message>valid</message>";
+		} else {
+			message = "<message>invalid</message>";
+		}
+		return new TextStreamResponse("type/xml", message);
+	}
+	
+	/**
+	 * @return the prototype
+	 */
+	public Asset getPrototype() {
+		return _prototype;
+	}
+
+	/**
+	 * @param prototype
+	 *            the prototype to set
+	 */
+	public void setPrototype(Asset prototype) {
+		_prototype = prototype;
+	}
+}
