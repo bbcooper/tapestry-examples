17c7b03f7da553b1f3aa27abba935d77e3b6cdc6
borut.bolcina <borut.bolcina@ac6f06fe-ab3b-0410-aaaf-636bd12ddef9>
2007-10-09 21:40:00 +0000

    Action method.

--- src/main/java/org/example/tapestry/pages/ui/dialog/ThickBoxDialogOnLoad.java
+++ src/main/java/org/example/tapestry/pages/ui/dialog/ThickBoxDialogOnLoad.java
@@ -1,10 +1,17 @@
 package org.example.tapestry.pages.ui.dialog;
 
 import org.apache.tapestry.Asset;
+import org.apache.tapestry.ComponentResources;
+import org.apache.tapestry.Link;
+import org.apache.tapestry.StreamResponse;
 import org.apache.tapestry.annotations.Inject;
 import org.apache.tapestry.annotations.Path;
+import org.apache.tapestry.util.TextStreamResponse;
 
 public class ThickBoxDialogOnLoad {
+	@Inject
+	private ComponentResources _resources;
+	
 	@Inject
 	@Path("context:assets/jquery-1.2.1.js")
 	private Asset _jquery;
@@ -17,27 +24,35 @@ public class ThickBoxDialogOnLoad {
 	@Path("context:assets/thickbox/thickbox.css")
 	private Asset _thickboxCSS;
 
-	public Asset getJquery() {
-		return _jquery;
+	/**
+	 * Generates a URI to the server-side function for the XHR to use.
+	 * 
+	 * @return the link
+	 */
+	public String getTheLink() {
+		//Link l = _resources.createActionLink("myAction", false);
+		Link l = _resources.createPageLink("ui/dialog/Step1", false);
+		return l.toURI();
 	}
 
-	public void setJquery(Asset _jquery) {
-		this._jquery = _jquery;
+	/**
+	 * This is a server-side method called via XHR that returns some text.
+	 * 
+	 * @return some text
+	 */
+	StreamResponse onMyAction() {
+		return new TextStreamResponse("text/html",
+				"<p>paragraph <b>bold</b></p>");
+	}
+	public Asset getJquery() {
+		return _jquery;
 	}
 
 	public Asset getThickbox() {
 		return _thickbox;
 	}
 
-	public void setThickbox(Asset _thickbox) {
-		this._thickbox = _thickbox;
-	}
-	
 	public Asset getThickboxCSS() {
 		return _thickboxCSS;
-	}
-
-	public void setThickboxCSS(Asset _thickboxCSS) {
-		this._thickboxCSS = _thickboxCSS;
 	}
 }
