d701d826af1e5ea2ebb35dee432f63f10b89987b
borut.bolcina <borut.bolcina@ac6f06fe-ab3b-0410-aaaf-636bd12ddef9>
2007-10-10 21:46:25 +0000

    Initial import.

---
+++ src/main/java/org/example/tapestry/pages/ui/dialog/JQModalWizard.java
@@ -0,0 +1,44 @@
+package org.example.tapestry.pages.ui.dialog;
+
+import org.apache.tapestry.Asset;
+import org.apache.tapestry.annotations.Inject;
+import org.apache.tapestry.annotations.Path;
+import org.apache.tapestry.annotations.Persist;
+
+public class JQModalWizard {
+	@Inject
+	@Path("context:assets/jquery-1.2.1.js")
+	private Asset _jquery;
+	
+	@Inject
+	@Path("context:assets/jqModal/jqModal.js")
+	private Asset jqmodal;
+	
+	@Inject
+	@Path("context:assets/jqModal/jqModal.css")
+	private Asset jqmodalCSS;
+
+
+	public Asset getJquery() {
+		return _jquery;
+	}
+
+	public Asset getJqmodal() {
+		return jqmodal;
+	}
+
+	public Asset getJqmodalCSS() {
+		return jqmodalCSS;
+	}
+	
+	@Persist
+	private String _currentPage;
+
+	void onStepChanged(String toPage) {
+		_currentPage = toPage;
+	}
+
+	void onActionFromStepChanged(String toPage) {
+		_currentPage = toPage;
+	}
+}
