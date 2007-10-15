769e36899a9effecd2905a4dd684222308055b54
borut.bolcina <borut.bolcina@ac6f06fe-ab3b-0410-aaaf-636bd12ddef9>
2007-10-15 14:04:27 +0000

    Initial import.

---
+++ trunk/src/main/java/org/example/tapestry/pages/ui/portlets/JQueryBasic.java
@@ -0,0 +1,47 @@
+package org.example.tapestry.pages.ui.portlets;
+
+import org.apache.tapestry.Asset;
+import org.apache.tapestry.annotations.Path;
+import org.apache.tapestry.ioc.annotations.Inject;
+
+public class JQueryBasic {
+    @Inject
+    @Path("context:assets/jquery-portlets/jquery.js")
+    private Asset jquery;
+    
+    @Inject
+    @Path("context:assets/jquery-portlets/interface.js")
+    private Asset jqinterface;
+    
+    @Inject
+    @Path("context:assets/jquery-portlets/config.js")
+    private Asset jqconfig;
+    
+    @Inject
+    @Path("context:assets/jquery-portlets/portlets.css")
+    private Asset jqportletsCSS;
+
+    @Inject
+    @Path("context:assets/jquery-portlets/logo.gif")
+    private Asset logo;
+
+    public Asset getJquery() {
+        return jquery;
+    }
+
+    public Asset getJqinterface() {
+        return jqinterface;
+    }
+    
+    public Asset getJqconfig() {
+        return jqconfig;
+    }
+
+    public Asset getJqportletsCSS() {
+        return jqportletsCSS;
+    }
+
+    public Asset getLogo() {
+        return logo;
+    }
+}
